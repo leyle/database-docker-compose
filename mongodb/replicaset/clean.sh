e96692252f729e6540e8dae4ac0b511c0f536216
leyle <leyle@leyle.com>
2022-07-15 13:55:45 +0800

    set mongodb config file

--- mongodb/replicaset/clean.sh
+++ mongodb/replicaset/clean.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-export TAG=4.4
-export NETWORK=mongodb-dev
-export TIMEZONE=Asia/Shanghai
-export ROOTUSER=rootuser
-export ROOTPASSWD=rootpasswd
-export PORT=27017
-export CONTAINER=mongodb-$PORT
-
-export DBUSER=dbuser
-export DBPASSWD=dbpasswd
-export DBDEV=dev
+. ./env.sh
 
 docker-compose -f mongodb.yaml down -v
 
--- mongodb/replicaset/start.sh
+++ mongodb/replicaset/start.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-export TAG=5.0
-export NETWORK=mongodb-dev
-export TIMEZONE=Asia/Shanghai
-export ROOTUSER=rootuser
-export ROOTPASSWD=rootpasswd
-export PORT=27017
-export CONTAINER=mongodb-$PORT
+. ./env.sh
 
-export REPLICA_SET_NAME=devRepl
-
-export DBUSER=dbuser
-export DBPASSWD=dbpasswd
-export DBDEV=dev
+KEY_FILE=$PWD/keyfile/dev.key
+if [ ! -f $KEY_FILE ]; then
+    echo "No $KEY_FILE"
+    exit 1
+fi
 
 DEBUG=$1
 if [ -z $DEBUG ]; then
