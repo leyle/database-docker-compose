f990fcd2a5803c761f76d09011ca4f82d22af03b
leyle <leyle@leyle.com>
2022-07-18 18:07:56 +0800

    update mongodb test scripts

--- mongodb/test.start-3-nodes.sh
+++ mongodb/test.start-3-nodes.sh
@@ -6,12 +6,14 @@ echo "start a mongodb replica set contains 3 nodes"
 DOMAIN_SUFFIX=".dev.pymom.com"
 BASE_PORT=27017
 
-BASE=$HOME/.config/replicaset
-mkdir -p $BASE
+WORK_DIR=$HOME/.config/replicaset
+mkdir -p $WORK_DIR
+cp -r ./replicaset $WORK_DIR/base
+BASE_SCRIPTS_DIR=$WORK_DIR/base
 
 # generate key file
 KEY_FILE_NAME="dev.key"
-KEY_FILE=$BASE/$KEY_FILE_NAME
+KEY_FILE=$WORK_DIR/$KEY_FILE_NAME
 echo "generate keyFile"
 openssl rand -base64 756 > $KEY_FILE
 
@@ -20,8 +22,9 @@ NUM=3
 end_idx=$((NUM - 1))
 for idx in $(seq 0 $end_idx);
 do
-    dst=$BASE/mgo${idx}
-    cp -r ./replicaset $dst
+    cd $WORK_DIR
+    dst=$WORK_DIR/mgo${idx}
+    cp -r  $BASE_SCRIPTS_DIR $dst
     cp $KEY_FILE $dst/keyfile/$KEY_FILE_NAME
 
     # set env.sh
