ade61f978997bd3d89b41b99036ee3963bfa7cc3
leyle <leyle@leyle.com>
2023-10-10 13:40:20 +0800

    upgrade redis to 7.2; downgrade mongodb to 4.4 due to lock AVX

--- redis/stop.sh
+++ redis/stop.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export TAG=6.0
-export NETWORK=redis-dev
-export TIMEZONE=Asia/Shanghai
-export PASSWORD=abc123
-export PORT=6379
-export CONTAINER=redis-server-$PORT
+source ./env.sh
 
 docker-compose -f redis.yaml down
