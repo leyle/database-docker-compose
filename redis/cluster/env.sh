9005a7bed90727a1f87ee81aa8163e9328dc8c2f
leyle <leyle@leyle.com>
2024-09-27 11:53:18 +0800

    update redis cluster to 3master and 3replica nodes

--- redis/cluster/env.sh
+++ redis/cluster/env.sh
@@ -6,3 +6,6 @@ export IP=192.168.88.134
 export PORT1=6379
 export PORT2=6380
 export PORT3=6381
+export PORT4=6382
+export PORT5=6383
+export PORT6=6384
--- redis/cluster/start.sh
+++ redis/cluster/start.sh
@@ -4,4 +4,4 @@
 
 docker-compose -f docker-3nodes.yaml up -d 
 
-docker run --rm -it --network host redis:$VERSION redis-cli --cluster create $IP:6379 $IP:6380 $IP:6381 --cluster-replicas 0 --cluster-yes
+docker run --rm -it --network host redis:$VERSION redis-cli --cluster create $IP:6379 $IP:6380 $IP:6381 $IP:6382 $IP:6383 $IP:6384 --cluster-replicas 1 --cluster-yes
