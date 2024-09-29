517137820a0e60668128a20c011ac9a961fc4082
leyle <leyle@leyle.com>
2024-09-29 10:53:31 +0800

    update redis script

--- redis/cluster/env.sh
+++ redis/cluster/env.sh
@@ -2,6 +2,7 @@
 #
 
 export VERSION=7.2
+export REDIS_PASSWORD=abc123
 export IP=192.168.88.134
 export PORT1=6379
 export PORT2=6380
--- redis/cluster/redis-cli.sh
+++ redis/cluster/redis-cli.sh
@@ -4,4 +4,4 @@
 
 # docker run --rm -it --network host redis:$VERSION redis-cli -c -h $IP -p 6379
 
-docker exec -it redis-cli redis-cli -c -h $IP -p 6379
+docker exec -it redis-cli redis-cli -c -h $IP -p 6379 -a $REDIS_PASSWORD
